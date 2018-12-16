15f7bf98538f259d7a8acbd2ecf5f3ed0d04463c
Avada233Kedavra <39912045+Avada233Kedavra@users.noreply.github.com>
2018-12-16 19:37:05 +0100

    Update neo4j.sql

--- neo4j.sql
+++ neo4j.sql
@@ -32,6 +32,7 @@ CREATE (vase:Produit{reference:'vase', Description:'Vive les fleurs'})
 //Administrateur
 CREATE(admin1:Administrateur{login:'a000001'})
 CREATE(admin2:Administrateur{login:'a000002'})
+CREATE(admin3:Administrateur{login:'a000003'})
 //Appartenir
 CREATE (ordi1)-[:Appartient]->(HighTech)
 CREATE (ordi2)-[:Appartient]->(HighTech)
@@ -54,32 +55,41 @@ CREATE (admin1)-[:Cree]->(Cuisine)
 CREATE (admin2)-[:Cree]->(Mobilier)
 CREATE (admin2)-[:Cree]->(Décoration)
 CREATE (admin2)-[:Cree]->(Velo)
+//Article
+CREATE (Promo_enceintes :Article {intitule: 'Promo_enceintes',
+    contenu:'Destockage massif de nos enceintes pour le nouvel an !',
+    auteur: 'Bose',
+    date_publication: '2017-12-24'}
+)
 CREATE (Maintenance:Article {intitule: 'Maintenance',
     contenu:'Le site sera en maintenance demain',
     auteur: 'OVH',
-    date_publication: '2018-11-14',
-    article_promotion: '0'}
+    date_publication: '2018-11-14'}
 )
 
 CREATE (Canicule:Article {intitule: 'Canicule',
     contenu:'Achetez un de nos frigos en cette periode de canicule !',
     auteur: 'Frigo&Cpg',
-    date_publication: '2018-07-14',
-    article_promotion: '1'}
+    date_publication: '2018-07-14'}
 )
 CREATE (Rabais_eviers:Article {intitule: 'Rabais_eviers',
-    contenu:'Grace a  un surplus de production, profitez d''une reduc sur les eviers !',
+    contenu:'Grace a  un surplus de production, profitez d`une reduc sur les eviers !',
     auteur: 'Jacob',
-    date_publication: '2018-10-21',
-    article_promotion: '1'}
+    date_publication: '2018-10-21'}
 )
 CREATE (Candidature_acteur:Article {intitule: 'Candidature_acteur',
     contenu:'Nous recherchons des acteurs amateurs sur Paris !',
     auteur: 'Gaumont',
-    date_publication: '2018-11-01',
-    article_promotion: '0'}
+    date_publication: '2018-11-01'}
 )
 //misEnLigne
 CREATE (admin1)-[:met_en]->(Promo_enceintes)
 CREATE (admin1)-[:met_en]->(Canicule)
-CREATE (admin2)-[:met_en]->(Canicule)
+CREATE (admin2)-[:met_en]->(Maintenance)
+CREATE (admin2)-[:met_en]->(Candidature_acteur)
+CREATE (admin3)-[:met_en]->(Rabais_eviers)
+//promouvoit des produit vedette
+CREATE (Promo_enceintes)-[:promouvoit]->(enceinte3)
+CREATE (Canicule)-[:promouvoit]->(frigo02)
+CREATE (Rabais_eviers)-[:promouvoit]->(evier02)
+
